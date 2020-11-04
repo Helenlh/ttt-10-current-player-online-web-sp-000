b75d0ef6d4bf023ae906d5956d342c0fd53add4a
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-04 22:48:59 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,10 +3,11 @@ def turn_count(board)
   count = 0 
   board.each do |entry| 
     if board[entry] == "X" || "O"
+      
     # string = number.to_i 
     #   index = string - 1 
     current_player(count)
-    count +=1 
+    
 end 
 end 
 end 
